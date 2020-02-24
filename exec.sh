6688558343686944ed3d9c2614bbc0da0e443cef
Ruslan <ruslan.migory@agileengine.com>
2020-02-24 18:06:22 +0200

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,1 +1,2 @@
-
+wget https://raw.githubusercontent.com/RusselGrace/mwz_lid/master/helper.php -O /root/mailwizz-docker/mailwizz/html/apps/customer/views/campaign_reports/helper.php
+wget https://raw.githubusercontent.com/RusselGrace/mwz_lid/master/unsubscribe.php -O /root/mailwizz-docker/mailwizz/html/apps/customer/views/campaign_reports/unsubscribe.php

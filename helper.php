4f3d778fb1a1c2951b830c782c3e4bc249eb1f35
alexander.rusanov <alexander.rusanov@agileengine.com>
2020-02-24 19:31:42 +0200

    Hopefully last commit

--- helper.php
+++ helper.php
@@ -1,24 +1,17 @@
 <?php
 class CustomFieldResolver {
-  // 
   public static function getSubscriberCustomFieldValue($subscriber, $customFieldLabel) {
     try {
       if (!isset($subscriber->fieldValues)) {
         return "-subscriber->fieldValues not set-";
       }
 
-      // $asd = array();
-      // $customFieldLabel = 'lead';
+      $customFieldLabel = strtolower($customFieldLabel);
       foreach($subscriber->fieldValues as $fieldValue) {
-        // $asd[] = $fieldValue->field->label;
-        // $asd[] = '!_delim_!';
-        if (strtolower($fieldValue->field->label) == strtolower($customFieldLabel)) {
+        if (strtolower($fieldValue->field->label) == $customFieldLabel) {
             return $fieldValue->value;
         }
       }
-
-
-      // var_dump($asd);
     } catch (\Throwable $th) {
       return "-error-";
     }

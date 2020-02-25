a9e869131dec46173e66e863f782f59ec0b0a132
Ruslan <ruslan.migory@agileengine.com>
2020-02-25 13:29:33 +0200

    Update bounce.php

--- bounce.php
+++ bounce.php
@@ -125,8 +125,8 @@ if ($viewCollection->renderContent) { ?>
                             'filter'=> false,
                         ),                        
                         array(
-                            'name'  => 'LEAD_ID',
-                            'value' => 'CustomFieldResolver::getSubscriberCustomFieldValue($data->subscriber, "LEAD_ID")',
+                            'name'  => 'LEADID',
+                            'value' => 'CustomFieldResolver::getSubscriberCustomFieldValue($data->subscriber, "LEADID")',
                             'filter' => false
                         ),
                         array(

46f1c6b80b79c87ac3ee522471a24ca4e25eb1cd
Ruslan <ruslan.migory@agileengine.com>
2020-06-11 18:44:57 +0300

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -9,5 +9,4 @@ wget https://raw.githubusercontent.com/RusselGrace/mwz_lid/master/Delivery_serve
 wget https://raw.githubusercontent.com/RusselGrace/mwz_lid/master/ListsController.php -O /root/mailwizz-docker/mailwizz/html/apps/api/controllers/ListsController.php
 wget https://raw.githubusercontent.com/RusselGrace/mwz_lid/master/RequestAccessFilter.php -O /root/mailwizz-docker/mailwizz/html/apps/api/components/web/filters/RequestAccessFilter.php
 wget https://raw.githubusercontent.com/RusselGrace/mwz_lid/master/init-custom.php -O /root/mailwizz-docker/mailwizz/html/apps/init-custom.php
-
 rm -f exec.sh

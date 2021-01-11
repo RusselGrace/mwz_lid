bee567f58b43d34f0136cacd8042ddb21b2258dc
Ruslan <ruslan.migory@agileengine.com>
2021-01-11 14:02:33 +0200

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,6 +1,7 @@
 wget https://raw.githubusercontent.com/RusselGrace/mwz_lid/master/helper.php -O /root/mailwizz-docker/mailwizz/html/apps/customer/views/campaign_reports/helper.php
 wget https://raw.githubusercontent.com/RusselGrace/mwz_lid/master/unsubscribe.php -O /root/mailwizz-docker/mailwizz/html/apps/customer/views/campaign_reports/unsubscribe.php
 wget https://raw.githubusercontent.com/RusselGrace/mwz_lid/master/open.php -O /root/mailwizz-docker/mailwizz/html/apps/customer/views/campaign_reports/open.php
+wget https://raw.githubusercontent.com/RusselGrace/mwz_lid/master/complain.php -O /root/mailwizz-docker/mailwizz/html/apps/customer/views/campaign_reports/complain.php
 wget https://raw.githubusercontent.com/RusselGrace/mwz_lid/master/open-unique.php -O /root/mailwizz-docker/mailwizz/html/apps/customer/views/campaign_reports/open-unique.php
 wget https://raw.githubusercontent.com/RusselGrace/mwz_lid/master/bounce.php -O /root/mailwizz-docker/mailwizz/html/apps/customer/views/campaign_reports/bounce.php
 wget https://raw.githubusercontent.com/RusselGrace/mwz_lid/master/Tracking_domains_aeController.php -O /root/mailwizz-docker/mailwizz/html/apps/api/controllers/Tracking_domains_aeController.php 
